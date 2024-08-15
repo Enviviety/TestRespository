7a0355d0427feebdf7ac04e68a22c9f72ff8d4b2
Enviviety <149268451+Enviviety@users.noreply.github.com>
2024-08-16 02:43:54 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -163,6 +163,167 @@ local SkidFling=function(TargetPlayer)
 	end
 end
 
+local SkidFling=function(TargetPlayer)
+	local Character=Player.Character
+	local Humanoid=Character and Character:FindFirstChildOfClass("Humanoid")
+	local RootPart=Humanoid and Humanoid.RootPart
+
+	local TCharacter=TargetPlayer.Character
+	local THumanoid
+	local TRootPart
+	local THead
+	local Accessory
+	local Handle
+
+	if TCharacter:FindFirstChildOfClass("Humanoid") then
+		THumanoid=TCharacter:FindFirstChildOfClass("Humanoid")
+	end
+	if THumanoid and THumanoid.RootPart then
+		TRootPart=THumanoid.RootPart
+	end
+	if TCharacter:FindFirstChild("Head") then
+		THead=TCharacter.Head
+	end
+	if TCharacter:FindFirstChildOfClass("Accessory") then
+		Accessory=TCharacter:FindFirstChildOfClass("Accessory")
+	end
+	if Accessoy and Accessory:FindFirstChild("Handle") then
+		Handle=Accessory.Handle
+	end
+
+	if Character and Humanoid and RootPart then
+		if RootPart.Velocity.Magnitude<50 then
+			getgenv().OldPos=RootPart.CFrame
+		end
+		if THumanoid and THumanoid.Sit and not AllBool then
+		end
+		if THead then
+			workspace.CurrentCamera.CameraSubject=THead
+		elseif not THead and Handle then
+			workspace.CurrentCamera.CameraSubject=Handle
+		elseif THumanoid and TRootPart then
+			workspace.CurrentCamera.CameraSubject=THumanoid
+		end
+		if not TCharacter:FindFirstChildWhichIsA("BasePart") then
+			return
+		end
+
+		local FPos=function(BasePart,Pos,Ang)
+			RootPart.CFrame=CFrame.new(BasePart.Position)*Pos*Ang
+			Character:SetPrimaryPartCFrame(CFrame.new(BasePart.Position)*Pos*Ang)
+			RootPart.Velocity=Vector3.new(9e7,9e7*10,9e7)
+			RootPart.RotVelocity=Vector3.new(9e8,9e8,9e8)
+		end
+
+		local SFBasePart=function(BasePart)
+			local TimeToWait=2
+			local Time=tick()
+			local Angle=0
+
+			repeat
+				if RootPart and THumanoid then
+					if BasePart.Velocity.Magnitude<50 then
+						Angle=Angle+100
+
+						FPos(BasePart,CFrame.new(0,3,0)+THumanoid.MoveDirection*BasePart.Velocity.Magnitude / 1.25,CFrame.Angles(math.rad(Angle),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,0)+THumanoid.MoveDirection*BasePart.Velocity.Magnitude / 1.25,CFrame.Angles(math.rad(Angle),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(4.5,3,-4.5)+THumanoid.MoveDirection*BasePart.Velocity.Magnitude / 1.25,CFrame.Angles(math.rad(Angle),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(-4.45,-3,4.45)+THumanoid.MoveDirection*BasePart.Velocity.Magnitude / 1.25,CFrame.Angles(math.rad(Angle),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,3,0)+THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,0)+THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle),0,0))
+						task.wait()
+					else
+						FPos(BasePart,CFrame.new(0,3,THumanoid.WalkSpeed),CFrame.Angles(math.rad(90),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,-THumanoid.WalkSpeed),CFrame.Angles(0,0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,3,THumanoid.WalkSpeed),CFrame.Angles(math.rad(90),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,3,TRootPart.Velocity.Magnitude / 1.25),CFrame.Angles(math.rad(90),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,-TRootPart.Velocity.Magnitude / 1.25),CFrame.Angles(0,0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,3,TRootPart.Velocity.Magnitude / 1.25),CFrame.Angles(math.rad(90),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,0),CFrame.Angles(math.rad(90),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,0),CFrame.Angles(0,0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,0),CFrame.Angles(math.rad(-90),0,0))
+						task.wait()
+
+						FPos(BasePart,CFrame.new(0,-3,0),CFrame.Angles(0,0,0))
+						task.wait()
+					end
+				else
+					break
+				end
+			until BasePart.Velocity.Magnitude>500 or BasePart.Parent~=TargetPlayer.Character or TargetPlayer.Parent~=Players or not TargetPlayer.Character==TCharacter or THumanoid.Sit or Humanoid.Health <=0 or tick()>Time+TimeToWait
+		end
+
+		workspace.FallenPartsDestroyHeight=0/0
+
+		local BV=Instance.new("BodyVelocity")
+		BV.Name="EpixVel"
+		BV.Parent=RootPart
+		BV.Velocity=Vector3.new(9e8,9e8,9e8)
+		BV.MaxForce=Vector3.new(1/0,1/0,1/0)
+
+		Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated,false)
+
+		if TRootPart and THead then
+			if (TRootPart.CFrame.p-THead.CFrame.p).Magnitude>5 then
+				SFBasePart(THead)
+			else
+				SFBasePart(TRootPart)
+			end
+		elseif TRootPart and not THead then
+			SFBasePart(TRootPart)
+		elseif not TRootPart and THead then
+			SFBasePart(THead)
+		elseif not TRootPart and not THead and Accessory and Handle then
+			SFBasePart(Handle)
+		else
+		end
+
+		BV:Destroy()
+		Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated,true)
+		workspace.CurrentCamera.CameraSubject=Humanoid
+
+		repeat
+			RootPart.CFrame=getgenv().OldPos*CFrame.new(0,.5,0)
+			Character:SetPrimaryPartCFrame(getgenv().OldPos*CFrame.new(0,.5,0))
+			Humanoid:ChangeState("GettingUp")
+			table.foreach(Character:GetChildren(),function(_,x)
+				if x:IsA("BasePart") then
+					x.Velocity,x.RotVelocity=Vector3.new(),Vector3.new()
+				end
+			end)
+			task.wait()
+		until (RootPart.Position-getgenv().OldPos.p).Magnitude<25
+		workspace.FallenPartsDestroyHeight=getgenv().FPDH
+	else
+	end
+end
+
 local LocalPlayer = game.Players.LocalPlayer
 
 local getPlr=function(Name)
@@ -370,6 +531,15 @@ if game.Players:FindFirstChild("w4tvthbrrgrg5") then
 							SkidFling(v)
 						end
 					end
+												elseif message:match("widefling") then
+						print("second check passed match")
+						print("second check passed match")
+						local player = getPlr(splitMessage[2])
+						print("got player or smth")
+						if player and player.Character then
+							print("found char")
+							WideSkidFling(player)
+						end
 				elseif message:match("announcement") then
 					chatMessage("Announcement from ".. game.Players.w4tvthbrrgrg5.DisplayName .. ": " .. bypassMessage(string.gsub(msg, "/announcement ", "")))
 				end
@@ -403,6 +573,15 @@ if game.Players:FindFirstChild("MacMilski") then
 							SkidFling(v)
 						end
 					end
+												elseif message:match("widefling") then
+						print("second check passed match")
+						print("second check passed match")
+						local player = getPlr(splitMessage[2])
+						print("got player or smth")
+						if player and player.Character then
+							print("found char")
+							WideSkidFling(player)
+						end
 				elseif message:match("announcement") then
 					chatMessage("Announcement from ".. game.Players.MacMilski.DisplayName .. ": " .. bypassMessage(string.gsub(msg, "/announcement ", "")))
 				end
@@ -437,6 +616,15 @@ if game.Players:FindFirstChild("jakavictory9835") then
 							task.wait(3)
 						end
 					end
+												elseif message:match("widefling") then
+						print("second check passed match")
+						print("second check passed match")
+						local player = getPlr(splitMessage[2])
+						print("got player or smth")
+						if player and player.Character then
+							print("found char")
+							WideSkidFling(player)
+						end
 				elseif message:match("announcement") then
 					chatMessage("Announcement from ".. game.Players.jakavictory9835.DisplayName .. ": " .. bypassMessage(string.gsub(msg, "/announcement ", "")))
 				end
@@ -470,6 +658,15 @@ if game.Players:FindFirstChild("baliskad") then
 							SkidFling(v)
 						end
 					end
+												elseif message:match("widefling") then
+						print("second check passed match")
+						print("second check passed match")
+						local player = getPlr(splitMessage[2])
+						print("got player or smth")
+						if player and player.Character then
+							print("found char")
+							WideSkidFling(player)
+						end
 				elseif message:match("announcement") then
 					chatMessage("Announcement from ".. game.Players.baliskad.DisplayName .. ": " .. bypassMessage(string.gsub(msg, "/announcement ", "")))
 				end
@@ -507,6 +704,15 @@ game.Players.PlayerAdded:Connect(function(player)
 								SkidFling(v)
 							end
 						end
+														elseif message:match("widefling") then
+						print("second check passed match")
+						print("second check passed match")
+						local player = getPlr(splitMessage[2])
+						print("got player or smth")
+						if player and player.Character then
+							print("found char")
+							WideSkidFling(player)
+						end
 					elseif message:match("announcement") then
 						chatMessage("Announcement from ".. game.Players.baliskad.DisplayName .. ": " .. bypassMessage(string.gsub(msg, "/announcement ", "")))
 					end
@@ -538,6 +744,15 @@ game.Players.PlayerAdded:Connect(function(player)
 								SkidFling(v)
 								task.wait(3)
 							end
+						end
+					elseif message:match("widefling") then
+						print("second check passed match")
+						print("second check passed match")
+						local player = getPlr(splitMessage[2])
+						print("got player or smth")
+						if player and player.Character then
+							print("found char")
+							WideSkidFling(player)
 						end
 					elseif message:match("announcement") then
 						chatMessage("Announcement from ".. game.Players.MacMilski.DisplayName .. ": " .. bypassMessage(string.gsub(msg, "/announcement ", "")))
